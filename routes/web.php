a188e04185816cf81fd8a063b2cb7b3cb90836cb
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-10-27 23:01:55 +0700

    latihan tugas pbo

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,6 @@ use App\Http\Controllers\Auth\{
 use App\Http\Controllers\BukuController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::redirect('/', '/buku');
 
 Route::middleware(['auth', 'verified'])->group(function () {
