7de85448138639924b6a7dae3f38876b91e7b7ac
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-10-30 17:58:07 +0700

    membuat api action untuk insert data buku

--- app/Http/Controllers/API/BukuController.php
+++ app/Http/Controllers/API/BukuController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Buku;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class BukuController extends Controller
 {
@@ -29,7 +30,37 @@ class BukuController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $attr = $request->only([
+                'judul_buku',
+                'penerbit',
+                'penulis',
+                'id_kategori',
+                'jumlah_halaman',
+            ]);
+
+            $validator = Validator::make($attr, [
+                'judul_buku' => ['required'],
+                'penerbit' => ['required'],
+                'penulis' => ['required'],
+                'id_kategori' => ['required'],
+                'jumlah_halaman' => ['required'],
+                'gambar' => ['image', 'mimes:png,jpg,jpeg,webp', 'max:2048']
+            ]);
+
+            if ($request->hasFile('gambar')) {
+                $attr['gambar'] = $request->file('gambar')->store('gambar_buku');
+            }
+
+            if ($validator->fails()) {
+                return ResponseJson::error('error', 'Failed to insert data', $validator->errors()->all());
+            }
+
+            $buku = Auth::user()->buku()->create($attr);
+            return ResponseJson::success('success', 'Success to insert data', $buku);
+        } catch (\Exception $e) {
+            return ResponseJson::error('error', 'Something went wrong', $e->getMessage());
+        }
     }
 
     /**
