69bc75babcf7a264e238cd61590fde40339cb81c
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-10-31 04:03:58 +0700

    perbaikan indentasi pasa class helper ResponseJson

--- app/Helpers/ResponseJson.php
+++ app/Helpers/ResponseJson.php
@@ -4,21 +4,21 @@ namespace App\Helpers;
 
 class ResponseJson
 {
-  public static function successWithToken($message = '', $data = null, $token = '', $statusCode = 200)
-  {
-    $type = 'success';
-    return response()->json(compact('type', 'message', 'data', 'token'), $statusCode);
-  }
+    public static function successWithToken($message = '', $data = null, $token = '', $statusCode = 200)
+    {
+        $type = 'success';
+        return response()->json(compact('type', 'message', 'data', 'token'), $statusCode);
+    }
 
-  public static function success($message = '', $data = null, $statusCode = 200)
-  {
-    $type = 'success';
-    return response()->json(compact('type', 'message', 'data'), $statusCode);
-  }
+    public static function success($message = '', $data = null, $statusCode = 200)
+    {
+        $type = 'success';
+        return response()->json(compact('type', 'message', 'data'), $statusCode);
+    }
 
-  public static function error($message = '', $error = null, $statusCode = 401)
-  {
-    $type = 'error';
-    return response()->json(compact('type', 'message', 'error'), $statusCode);
-  }
+    public static function error($message = '', $error = null, $statusCode = 401)
+    {
+        $type = 'error';
+        return response()->json(compact('type', 'message', 'error'), $statusCode);
+    }
 }
